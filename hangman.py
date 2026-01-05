aec7c356536d2fabeff009c0187666aa7520fd8b
Harsha vardhan S <harshas1401@gmail.com>
2026-01-05 22:23:39 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,18 @@
+import random
+
+word_list = ["aardvark", " baboon" , "camel" ]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+placeholder = ""
+word_length = len(chosen_word)
+for position in range (word_length):
+    placeholder += "_"
+print(placeholder)
+guess = input("Guess a latter : ").lower()
+print(guess)
+
+for letter in chosen_word:
+    if letter == guess:
+        print(guess)
+    else:
+        print("_")   

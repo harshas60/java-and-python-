c5e2680a89482e6548825b781d806130ea993539
Harsha vardhan S <harshas1401@gmail.com>
2026-01-08 21:50:38 +0530

    Add files via upload

---
+++ ceasar_cipher.py
@@ -0,0 +1,12 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_index = (alphabet.index(letter) + shift_amount)
+        cipher_text += alphabet[shifted_index]
+    print(f"The encoded text is: {cipher_text}")
+encrypt
